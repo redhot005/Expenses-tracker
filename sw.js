7a7817ac9980bf5d16514f7c47b95b0ae9c214d3
redhot005 <rchojilla@gmail.com>
2026-03-26 23:25:35 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-
+var CN='expenses-v14';
+var AS=['./', './index.html', './manifest.json'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CN).then(function(c){return c.addAll(AS)}));self.skipWaiting()});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CN}).map(function(k){return caches.delete(k)}))}));self.clients.claim()});
+self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request).then(function(r){var cl=r.clone();caches.open(CN).then(function(c){c.put(e.request,cl)});return r}).catch(function(){return caches.match(e.request)}))});
